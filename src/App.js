f757940a8b2992c620ec3f715f80d93c6de3816f
Yash Nikam <yashnikam184@gmail.com>
2024-05-01 18:02:09 +0530

    NewMsgadded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and Yash.
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Yash Nikam
+          Yash Ananda Nikam..
         </a>
       </header>
     </div>
